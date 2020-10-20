b0d35f6351619833c738bf99baf08e5af761e5da
Devin Hicks <devinhicks.az@gmail.com>
2020-10-19 23:42:34 -0700

    initial commit

---
+++ RelayPG/Assets/Tilemap/Characters.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Characters" tilewidth="32" tileheight="32" tilecount="168" columns="28">
+ <image source="../../../Multiplayer/RPG/RPG/Assets/Sprites/Characters.png" width="918" height="203"/>
+</tileset>
---
+++ RelayPG/Assets/Tilemap/Items.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="Items" tilewidth="32" tileheight="32" tilecount="441" columns="21">
+ <image source="../../../Multiplayer/RPG/RPG/Assets/Sprites/Items.png" width="692" height="692"/>
+</tileset>
